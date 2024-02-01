8b3859049c140a1e3a47e5c09920f38de4b1a7c1
Jorge Sánchez Fernández <124562609+JorgeSanchezF@users.noreply.github.com>
2024-02-01 17:25:56 +0100

    Add files via upload

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -5,9 +5,9 @@ import axios from "axios";
 const posts = ref([]);
 const favoritos = ref([]);
 const bool = ref(true);
+const postNuevo = ref({ titulo: "", texto: "" });
 
 function api() {
-  //manera de sergio, la otra no funciona?
   axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
     posts.value = response.data;
   });
@@ -24,6 +24,23 @@ function eliminarFavorito(post) {
   const index = favoritos.value.findIndex((fav) => fav.id === post.id);
   favoritos.value.splice(index, 1);
 }
+function crearPost() {
+  axios
+    .post("https://jsonplaceholder.typicode.com/posts", {
+      title: postNuevo.value.titulo,
+      body: postNuevo.value.texto,
+      userId: 1,
+    })
+    .then((response) => {
+      posts.value.unshift(response.data);
+      postNuevo.value.titulo = "";
+      postNuevo.value.texto = "";
+    });
+}
+function eliminarPost(post) {
+  const index = posts.value.findIndex((p) => p.id === post.id);
+  posts.value.splice(index, 1);
+}
 </script>
 
 <template>
@@ -35,6 +52,28 @@ function eliminarFavorito(post) {
       <h2 v-if="bool">Posts</h2>
       <h2 v-else>Favoritos</h2>
       <div class="row">
+        <form @submit.prevent="crearPost">
+          <div class="mb-3">
+            <label for="titulo" class="form-label">Título del Post:</label>
+            <input
+              type="text"
+              class="form-control"
+              id="titulo"
+              v-model="postNuevo.titulo"
+              required
+            />
+          </div>
+          <div class="mb-3">
+            <label for="body" class="form-label">Texto del Post:</label>
+            <textarea
+              class="form-control"
+              id="body"
+              v-model="postNuevo.body"
+              required
+            ></textarea>
+          </div>
+          <button type="submit" class="btn btn-success">Crear Post</button>
+        </form>
         <div
           v-if="bool"
           v-for="post in posts"
@@ -47,6 +86,9 @@ function eliminarFavorito(post) {
               <p class="card-text">{{ post.body }}</p>
               <button @click="agregarFavorito(post)" class="btn btn-info">
                 Añadir a Favoritos
+              </button>
+              <button @click="eliminarPost(post)" class="btn btn-danger">
+                Eliminar Post
               </button>
             </div>
           </div>
@@ -72,6 +114,4 @@ function eliminarFavorito(post) {
   </div>
 </template>
 
-<style>
-/* Puedes agregar estilos adicionales aquí si es necesario */
-</style>
+<style></style>
